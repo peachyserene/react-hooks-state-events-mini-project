1e03573bd38be3a66976a2d5624ee76b567465e4
Georgia Burnside <burnsidegeorgia@gmail.com>
2022-05-19 12:36:04 -0400

    commit tree fc7e50cad80864e7c3d846515019507f2fe74b1a

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  const [newName, setNewName] = useState("");
+  const [newCat, setNewCat] = useState("Code");
+
+  let catOptions = categories.map((item) => {
+    return item !== "All" ? <option key={item}>{item}</option> : null;
+  });
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    onTaskFormSubmit({ text: newName, category: newCat });
+  }
+  function handleNameChange(e) {
+    setNewName(e.target.value);
+  }
+  function handleCatChange(e) {
+    console.log(e.target.value);
+    setNewCat(e.target.value);
+  }
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleFormSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input
+          type="text"
+          name="text"
+          value={newName}
+          onChange={handleNameChange}
+        />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select name="category" value={newCat} onChange={handleCatChange}>
+          {catOptions}
         </select>
       </label>
       <input type="submit" value="Add task" />
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks, handleDelete }) {
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasks.map((task) => {
+        return (
+          <Task
+            key={task.text}
+            text={task.text}
+            category={task.category}
+            handleDelete={handleDelete}
+          />
+        );
+      })}
     </div>
   );
 }
