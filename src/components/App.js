27467220fbeae6c502782bbee7ed668b9f78c159
Georgia Burnside <burnsidegeorgia@gmail.com>
2022-05-19 12:36:23 -0400

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
-
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [newTaskList, setNewTaskList] = useState(TASKS);
+  const [selectedCat, setSelectedCat] = useState("All");
+  // const [newTaskItem, setNewTaskItem] = useState([]);
+
+  //removes item from list when delete button is clicked
+  function handleDelete(text) {
+    console.log(text);
+    setNewTaskList(
+      newTaskList.filter((item) => {
+        return item.text !== text;
+      })
+    );
+  }
+  //creates a new list with the added task
+  function updateTasksList(task) {
+    setNewTaskList([...newTaskList, task]);
+  }
+
+  //filters out the tasks that dont match the selected category
+  let visibleTasks = newTaskList.filter((item) => {
+    if (selectedCat === "All") {
+      return true;
+    }
+    return item.category === selectedCat;
+  });
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter
+        categories={CATEGORIES}
+        setSelectedCat={setSelectedCat}
+        selectedCat={selectedCat}
+      />
+      <NewTaskForm categories={CATEGORIES} onTaskFormSubmit={updateTasksList} />
+      <TaskList tasks={visibleTasks} handleDelete={handleDelete} />
     </div>
   );
 }
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,25 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories, setSelectedCat, selectedCat }) {
+  function handleClick(e) {
+    setSelectedCat(e.target.textContent);
+  }
+  //
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categories.map((category) => {
+        let selectedClass = category === selectedCat ? "selected" : "";
+        return (
+          <button
+            className={selectedClass}
+            onClick={handleClick}
+            key={category}
+          >
+            {category}
+          </button>
+        );
+      })}
     </div>
   );
 }
